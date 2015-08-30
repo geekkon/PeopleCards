ba7a996e811b31d99c16297646f76e21f9dffbda
Dim <neetro@mail.ru>
2015-08-30 18:04:58 +0400

    Initial Commit

---
+++ TestPersonProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPersonProject
+//
+//  Created by Dim on 30.08.15.
+//  Copyright (c) 2015 Dmitriy Baklanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
